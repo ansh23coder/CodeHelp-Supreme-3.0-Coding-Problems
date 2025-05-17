1124758432962b8d433451ad9c850e2d5aeedc68
Anupam Sharma <ansh151923@gmail.com>
2025-05-17 09:45:40 +0530

    adding 169leetcode problem
    Maority elements

---
+++ 0leetCode_HackerRank_Gfg/169MajorityEleements.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        sort(nums.begin(), nums.end());
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] == nums[i] || count <= 2) 
+                count++;
+        }
+        // if(count <= 2) 
+        return count;
+    }
+};
