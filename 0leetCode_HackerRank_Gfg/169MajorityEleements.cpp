664b9e41be985b741e8007c90e60baef818f21da
Anupam Sharma <ansh151923@gmail.com>
2025-05-17 10:00:01 +0530

    adding 2 problems

--- 0leetCode_HackerRank_Gfg/169MajorityEleements.cpp
+++ 0leetCode_HackerRank_Gfg/169MajorityEleements.cpp
@@ -1,16 +1,26 @@
+// https://leetcode.com/problems/majority-element/
+
 /*
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        // by using - Boyer-Moore Majority Vote algorithm
         int count = 0;
-        sort(nums.begin(), nums.end());
-
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] == nums[i] || count <= 2) 
+        int candidate;
+        for(int num: nums){
+            if(count == 0){
+                candidate = num;
+                count = 1;
+            }
+            else if(candidate == num){
                 count++;
-        }
-        // if(count <= 2) 
-        return count;
+            }
+            else{
+                count--;
+            }
+        } 
+        return candidate;
     }
 };
+// This algorithm works by essentially maintaining a counter for the majority element. If the counter is 0, we set the current element as the majority element and increment the counter. If the current element is the same as the majority element, we increment the counter. If the current element is different from the majority element, we decrement the counter.
 */
