c976b86e55ad2c751c93052d81208b3811e6a81a
Anupam Sharma <ansh151923@gmail.com>
2025-05-29 23:05:41 +0530

    updating code

--- Basic Maths and Pointers/GCDofTwoNumbers.cpp
+++ Basic Maths and Pointers/GCDofTwoNumbers.cpp
@@ -1,21 +1,18 @@
 // https://www.geeksforgeeks.org/problems/gcd-of-two-numbers3459/1
 
 /*
-
 class Solution {
   public:
     int gcd(int a, int b) {
         // code here
-        int result = min(a, b);
-        
-        while(result > 0){
-            if(a % result == 0 && b % result == 0) {
-                break;
-            }
-            result--;
+        if (b == 0) {
+            return a;
+        } 
+        else {
+            return gcd(b, a % b);
         }
     }
-    return result;
 };
+
 
 */
